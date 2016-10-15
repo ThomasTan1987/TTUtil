aeff2c796a747707c854f99b10512b6254d77b92
ThomasTan1987 <914964758@qq.com>
2016-10-15 21:05:22 +0800

    日志写入文件

--- Classes/TTUtil.h
+++ Classes/TTUtil.h
@@ -7,7 +7,10 @@
 //
 
 #import <Foundation/Foundation.h>
-
+@interface Status:NSObject
+@property(nonatomic,assign)BOOL result;
+@property(nonatomic,copy)NSString *message;
+@end
 @interface TTUtil : NSObject
 /**
  *  秒数转字符串
@@ -17,4 +20,20 @@
  *  @return 转换结果
  */
 + (NSString *)timeFormatter:(NSInteger)sec;
+/**
+ *  检验是否符合我们对手机号码的验证
+ *
+ *  @param phoneNumber 用户输入的手机号
+ *
+ *  @return 是否符合我们的要求
+ */
++ (Status *)checkPhoneNumber:(NSString*)phoneNumber;
+/**
+ *  密码规则
+ *
+ *  @param password 用户输入的密码
+ *
+ *  @return 是否符合我们的要求
+ */
++ (Status *)checkPassword:(NSString*)password;
 @end
--- Classes/TTUtil.m
+++ Classes/TTUtil.m
@@ -7,7 +7,8 @@
 //
 
 #import "TTUtil.h"
-
+@implementation Status
+@end
 @implementation TTUtil
 /**
  *  秒数转字符串
@@ -27,5 +28,65 @@
         return [NSString stringWithFormat:@"%02ld:%02ld",(long)minute,(long)second];
     }
     
+}
++ (Status *)checkPhoneNumber:(NSString*)phoneNumber
+{
+    Status *status = [[Status alloc] init];
+    //台湾10位，大陆11位
+    if (phoneNumber.length == 10 || phoneNumber.length == 11) {
+        
+    } else {
+        status.result = NO;
+        status.message = @"手机号输入错误，请重新输入";
+        return status;
+    }
+    //台湾0开头，大陆1开头
+    if ([phoneNumber hasPrefix:@"0"] || [phoneNumber hasPrefix:@"1"]) {
+        
+    } else {
+        status.result = NO;
+        status.message = @"手机号输入错误，请重新输入";
+        return status;
+    }
+    //仅限数字
+    for (int i = 0; i < phoneNumber.length; i ++) {
+        unichar c = [phoneNumber characterAtIndex:i];
+        if (c > 57 || c < 48) {//ascii 48～57表示数字0~9
+            status.result = NO;
+            status.message = @"手机号输入错误，请重新输入";
+            return status;
+        }
+    }
+    //
+    status.result = YES;
+    status.message = nil;
+    return status;
+}
++ (Status *)checkPassword:(NSString*)password
+{
+    Status *status = [[Status alloc] init];
+    //6~14位
+    if (password.length<6 || password.length>14) {
+        status.result = NO;
+        status.message = @"请输入6~14位数字或字母密码";
+        return status;
+    }
+    //仅限大小写字母、数字
+    for (int i = 0; i < password.length; i ++) {
+        unichar c = [password characterAtIndex:i];
+        //ascii 48～57表示数字0~9
+        //大写字母 65~90
+        //小写字母 97~122
+        if ((c < 58 && c > 47) || (c < 91 && c > 64) || (c < 123 && c > 96)) {
+            
+        } else {
+            status.result = NO;
+            status.message = @"密码包含非法字符，请输入6~14位数字或字母";
+            return status;
+        }
+    }
+    status.result = YES;
+    status.message = nil;
+    return status;
 }
 @end
